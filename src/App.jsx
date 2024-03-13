9f2702575f835781e8d3bdcc59db145368677cb9
nekokaito <siddiqsazzad001@gmail.com>
2024-03-13 11:25:21 +0600

    created react contries tour

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Countries from './Countries'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <Countries></Countries>
+    </>
+  )
+
+   }  
+  
+  
+
+
+export default App
